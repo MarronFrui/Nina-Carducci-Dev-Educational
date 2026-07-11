8ce743441932a17c7f6878318c3ea9c68ba50d56
MarronFrui <maxime.fels@gmail.com>
2026-07-11 22:41:06 +0200

    Feat: changed project Architecture to modern standard

--- src/scripts.js
+++ src/scripts.js
@@ -1,6 +1,5 @@
 import $ from 'jquery';
-import 'bootstrap/dist/js/bootstrap.bundle';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import '../scss/style.scss';
 import './maugallery.js';
 
 $(document).ready(function () {
