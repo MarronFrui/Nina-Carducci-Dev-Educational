3cffdb8c68fc3b49c9334ae5b06c247614e3af03
MarronFrui <maxime.fels@gmail.com>
2026-07-11 02:14:27 +0200

    WIP bootstrap 4

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,15 +1,20 @@
-$(document).ready(function() {
-    $('.gallery').mauGallery({
-        columns: {
-            xs: 1,
-            sm: 2,
-            md: 3,
-            lg: 3,
-            xl: 3
-        },
-        lightBox: true,
-        lightboxId: 'myAwesomeLightbox',
-        showTags: true,
-        tagsPosition: 'top'
-    });
+import $ from 'jquery';
+import 'bootstrap/dist/js/bootstrap.bundle';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './maugallery.js';
+
+$(document).ready(function () {
+  $('.gallery').mauGallery({
+    columns: {
+      xs: 1,
+      sm: 2,
+      md: 3,
+      lg: 3,
+      xl: 3,
+    },
+    lightBox: true,
+    lightboxId: 'myAwesomeLightbox',
+    showTags: true,
+    tagsPosition: 'top',
+  });
 });
